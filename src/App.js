f70f0d9633077ea3ce56fe8fcb580f08563ac13e
ONS <onshamadkl@gmail.com>
2024-03-18 20:19:21 +0100

    first

--- src/App.js
+++ src/App.js
@@ -6,12 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      
+        Ceci est le site test de ONS
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reactjs.org" 
           target="_blank"
           rel="noopener noreferrer"
         >
