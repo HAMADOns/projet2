743a2aea7f3d007ec7a76b6b2b5a8e071b10a66f
HAMADOns <onshamadkl@gmail.com>
2024-03-19 20:31:20 +0100

    format

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 
 import './App.css';
 import * as React from "react";
+import './style.css'
+
+const smallContainer = <div className="container" 
+        style={{color:'white', backgroundColor:'darkslateblue'}}>
+        Petit conteneur dark texte blanc</div>
+const mediumContainer = <div className="container" 
+        style={{color:'black', backgroundColor:'pink'}}>
+        Moyen conteneur rose texte noire</div>
+const largeContainer = <div className=  
 
 
 
+"container"  
+
+        style={{color:'black', backgroundColor:'green'}}>
+        Grand conteneur vert rose texte noire</div>
+ 
+function Container({ children,style, color}){  
+      return <div  
+      
+      style={{...style,border:'1px solid',color }}>{children}  </div>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-        Ceci est le site test de ONS
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org" 
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+      <header className="App-header">       
+        Ceci est le site test des styles       
       </header>
+      <Container color ='blue' style={{backgroundColor:'darkslateblue'}}>Ceci est un test</Container>
+      {smallContainer}
+      {mediumContainer}
+      {largeContainer}
     </div>
   );
 }
